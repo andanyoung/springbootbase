0eedbb5a82572b6bc2d62de07a2b366e0fbbe68b
andanyang <1218853253@qq.com>
2019-11-05 17:06:38 +0800

    add READNE

---
+++ README.md
@@ -0,0 +1,5 @@
+# springbootbase
+> springboot 基础工程 包含功能
+- 全局AOP日志，接口运行时间
+- 全局异常
+- 固定返回参数
--- src/main/java/cn/andyoung/base/common/beans/ResultBean.java
+++ src/main/java/cn/andyoung/base/common/beans/ResultBean.java
@@ -7,9 +7,9 @@ public class ResultBean<T> implements Serializable {
 
   public static final int NO_LOGIN = -1;
 
-  public static final int SUCCESS = 0;
+  public static final int SUCCESS = 10000;
 
-  public static final int FAIL = 1;
+  public static final int FAIL = 55555;
 
   public static final int NO_PERMISSION = 2;
 
